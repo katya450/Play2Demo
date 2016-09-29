1266745a096f027f6f0c54c5712b6d2feaccd19a
Katja Lusma <katja.lusma@gmail.com>
2016-09-29 17:35:05 +0300

    added return values for the front to work

--- app/controllers/ShoppingListController.java
+++ app/controllers/ShoppingListController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import models.Item;
-import play.*;
 import play.libs.Json;
 import play.mvc.*;
 import java.util.List;
@@ -10,31 +9,30 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 
 public class ShoppingListController extends Controller{
+	
+	private static Finder<Long, Item> find = new Finder<Long, Item>(Item.class);
 
-	   @BodyParser.Of(BodyParser.Json.class)
-       public Result addItem() {
-		  //Logger.info("addItem");
-		   JsonNode json = request().body().asJson(); 
-		   if(json == null) {
-			   return badRequest("Ei sopiva muoto!");
-		   }
-		   Item item = Json.fromJson(json, Item.class);
-		   Ebean.save(item);
-		   return ok();
-       }
+	@BodyParser.Of(BodyParser.Json.class)
+    public Result addItem() {
+		//Logger.info("addItem");
+		JsonNode json = request().body().asJson(); 
+		if(json == null) {
+			return badRequest("Ei sopiva muoto!");
+		}
+	    Item item = Json.fromJson(json, Item.class);
+		Ebean.save(item);
+		return ok(Json.toJson(item));
+	}
+    
+	public Result deleteItem(Long id) {
+		Ebean.createSqlUpdate("DELETE FROM item WHERE id=" + id).execute();
+		return ok(Json.toJson(id));
+   }
+   
+   public Result listItems() {	
+	   List<Item> items = find.all();
+	   return ok(Json.toJson(items));
+   }
        
-       public Result deleteItem(Long id) {
-    	   Ebean.createSqlUpdate("DELETE FROM item WHERE id=" + id).execute();
-    	   return ok();
-       }
-       
-       public Result listItems() {	
-    	    List<Item> items = find.all();
-
-    	   return ok(Json.toJson(items));
-       }
-       
-       public static Finder<Long, Item> find = 
-			      new Finder<Long, Item>(Item.class);
 }
 
